4b8bc9653138c7d90f007c81dc51953c9518e222
greenback12 <38668052+greenback12@users.noreply.github.com>
2018-10-16 01:05:42 -0400

    Add files via upload

---
+++ XbalanqueBot.py
@@ -0,0 +1,25 @@
+#Xbalanque bot by Senor
+
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    content = message.content.lower()
+    if content == "!speak":
+        await asyncio.sleep(1)
+        await client.send_message(message.channel, 'whachu wan gringo')
+    elif content == "!cuss":
+        await asyncio.sleep(1)
+        await client.send_message(message.channel, 'FUCK')
+
+client.run('NTAxNTgxNjYzMzIzNjg0OTM0.Dqbxvg.m0l00rmnkjMrOkDUBzb1-uNetDA')
